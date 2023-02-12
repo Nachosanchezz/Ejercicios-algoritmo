f6d9f39536efa07c0f57987690aac8c228c167eb
Nacho <isanccal@myuax.com>
2023-02-12 21:33:31 +0100

    Cambio 1

---
+++ Ejercicio9,2.py
@@ -0,0 +1,4 @@
+def calculated_weighted_mean(num1, num2, num3, weight1, weight2, weight3):
+    weighted_mean = (num1 * weight1 + num2 * weight2 + num3 * weight3) / (weight1 + weight2 + weight3)
+    return weighted_mean
+
