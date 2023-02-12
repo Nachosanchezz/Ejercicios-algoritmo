e14cb2b6304a170aaf5bbda352ae6ccfcf180866
Nacho <isanccal@myuax.com>
2023-02-12 22:09:36 +0100

    Cambio2

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,3 +1,12 @@
 def calculate_overtime_pay(monthly_salary, overtime_hours):
     weekly_salary = monthly_salary / 12 *52 / 35
     hourly_rate = weekly_salary / 35
+
+    if overtime_hours <= 8:
+        return 0
+    elif overtime_hours <= 36:
+        return (overtime_hours - 8) * hourly_rate
+    elif overtime_hours <= 43:
+        return (36 - 8) * hourly_rate + (overtime_hours - 36) * hourly_rate * 1.25
+    else: 
+        return (36 - 8) 
