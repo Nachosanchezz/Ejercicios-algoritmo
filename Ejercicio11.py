8842f258ae47366c92d05ad91a9fd01eb3a430bd
Nacho <isanccal@myuax.com>
2023-02-12 22:12:04 +0100

    Cambio final

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -9,4 +9,12 @@ def calculate_overtime_pay(monthly_salary, overtime_hours):
     elif overtime_hours <= 43:
         return (36 - 8) * hourly_rate + (overtime_hours - 36) * hourly_rate * 1.25
     else: 
-        return (36 - 8) 
+        return (36 - 8) * hourly_rate + (43 - 36) * hourly_rate * 1.25 + (overtime_hours - 43) * hourly_rate * 1.5
+    
+monthly_salary = float(input("Introduce el salario mensual: "))
+overtime_hours = float(input("Introduce las horas extras: "))
+result = calculate_overtime_pay(monthly_salary, overtime_hours)
+
+print("El importe de las horas extras es: ", result)
+
+
