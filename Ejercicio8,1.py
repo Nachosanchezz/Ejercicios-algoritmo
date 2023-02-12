3f5c52a91f67638f1e6878a0a82b83845f096fd0
Nacho <isanccal@myuax.com>
2023-02-12 21:17:46 +0100

    Cambio 2

--- Ejercicio8,1.py
+++ Ejercicio8,1.py
@@ -1,3 +1,8 @@
 def calculate_tii(price_without_taxes, vat_percentage):
-    vat_value = price_without_taxes * vat_percentage / 100 
-    
+    vat_value = price_without_taxes * vat_percentage / 100
+    tii = price_without_taxes + vat_value
+    return tii
+
+price_without_taxes = float(input("Introduce el precio sin impuestos: "))
+
+
