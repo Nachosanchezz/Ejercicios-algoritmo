53f26c81c3337ae3c6223ce535c33090bb91945e
Nacho <isanccal@myuax.com>
2023-02-12 21:30:20 +0100

    Cambio 2

--- Ejercicio9,1.py
+++ Ejercicio9,1.py
@@ -2,3 +2,11 @@ def calculate_mean(num1, num2, num3):
     mean = (num1 + num2 + num3) / 3
     return mean
 
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+num3 = float(input("Introduce el tercer número: "))
+
+mean = calculate_mean(num1, num2, num3)
+
+print("La media de los tres números es: ", mean)
+
